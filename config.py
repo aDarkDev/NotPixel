a22167ae07f438c756fbe6a25453d4c107b658a3
Dark Dev <121636230+aDarkDev@users.noreply.github.com>
2024-09-29 11:43:39 +0330

    Update to v2.0

---
+++ config.py
@@ -0,0 +1,16 @@
+# put yours
+API_ID = 123
+API_HASH = "123"
+
+# =============[upgrades]================
+# default is 5 level you can change it your self
+PAINT_REWARD_MAX = 5 # max is 7
+ENERGY_LIMIT_MAX = 5 # max is 6
+RE_CHARGE_SPEED_MAX = 5 # max is 11
+
+# ================[proxy]================
+USE_PROXY = False # or put True if you want use it
+PROXIES = {
+    "http":"socks5://127.0.0.1",
+    "https":"socks5://127.0.0.1", # if you using socks4 or http only replace it with "socks5"
+}
